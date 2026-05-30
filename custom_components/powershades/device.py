7b31a4097635925b2f27eef7df913c01d48fb24c
vemboy20 <vemboy20@gmail.com>
2026-05-29 21:06:18 -0700

    do i like have to move it up or something

--- custom_components/powershades/device.py
+++ custom_components/powershades/device.py
@@ -250,14 +250,14 @@ class PowerShadesDevice:
                      self.ip_address,
                  )
                  self.coordinator.update_interval = timedelta(seconds=2)
-         elif self._available and self._position is not None:
-             # Device available and position known - normal polling
-             if self.coordinator.update_interval.total_seconds() < 10:
-                 _LOGGER.debug(
-                     "Device %s position known, restoring normal polling",
-                     self.ip_address,
-                 )
-                 self.coordinator.update_interval = timedelta(seconds=10)
+          elif self._available and self._position is not None:
+              # Device available and position known - normal polling
+              if self.coordinator.update_interval.total_seconds() < 10:
+                  _LOGGER.debug(
+                      "Device %s position known, restoring normal polling",
+                      self.ip_address,
+                  )
+                  self.coordinator.update_interval = timedelta(seconds=10)
         
 
     async def _async_update_data(self):
